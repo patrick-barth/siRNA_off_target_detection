bc1aec82f0f6cf46a6a6fb9c4d7dc615e2318ba8
Patrick Barth <patrick.barth@computational.bio.uni-giessen.de>
2024-03-06 17:43:56 +0100

    add sequence parsing and start with start position determination

--- detect_functional_siRNAs.py
+++ detect_functional_siRNAs.py
@@ -6,12 +6,17 @@
 #  that could be derived from this sequence #
 #############################################
 
-#######################
-#######################
-###    variables    ###
-#######################
-#######################
+import argparse
+import os
+from typing import Dict, List
+from Bio import SeqIO
 
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--sequence', 	'-s', type=str,	help='Sequence to derive siRNAs from')
+parser.add_argument('--rules',      '-r', type=str, default='mammal', help='Rules to determine siRNAs')
+args = parser.parse_args()
 
 ########################
 ########################
@@ -19,6 +24,7 @@
 ########################
 ########################
 
+mammal_start_nuc_guide: List = ['A','T','U']
 
 ##########################################################################################################################################
 
@@ -29,8 +35,16 @@
 ###################
 
 
-def main():
-	print('hi')
+def main(sequence,rules):
+	sequence: Dict = SeqIO.parse(sequence,"fasta")
+	for entry in sequence:
+		start_positions: List = determine_siRNA_starts(entry.seq,rules)
+		print(start_positions)
+		print(entry.seq)
+
+		
+			
+
 
 #######################
 #######################
@@ -38,9 +52,18 @@ def main():
 #######################
 #######################
 
+def determine_siRNA_starts(sequence,rules):
+	start_positions: List = []
+	# For mammals siRNAs start with an A or U at position 1 of the guide strand
+	if rules == 'mammal': 
+		start_positions = [idx for idx, item in enumerate(sequence.lower()) if item in mammal_start_nuc_guide] #TODO: fix position getting
+
+	return(start_positions)
+
 
 
 ##########################
 ### starts main script ###
 ##########################
-main()
+main(sequence=args.sequence,
+	rules=args.rules)
