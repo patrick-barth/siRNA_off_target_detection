59ff0007e70ffd439543e30aa680ce5ca8b6463e
Patrick Barth <patrick.barth@computational.bio.uni-giessen.de>
2024-06-27 17:35:14 +0200

    finish data download and db building for both: NCBI and insectBase

--- build_db.sh
+++ build_db.sh
@@ -7,26 +7,39 @@
 DATE=$(date '+%Y-%m-%d_%H-%M-%S')
 WORK_DIR=$(pwd)
 TMP_DIR="${WORK_DIR}/tmp"
+TMP_DIR_GROUPS="${TMP_DIR}/groups"
+##TMP_DIR_NCBI="${TMP_DIR}/NCBI"
+##TMP_DIR_INSECTBASE="${TMP_DIR}/insectBase"
 OUTPUT_DIR="${WORK_DIR}/insect_genomes_database_$DATE/"
-#SPREADSHEET_LINK="https://docs.google.com/spreadsheets/d/1RIiyyyTIbz6HRRbVrbnf56rNVvgsLUk3FSJ4xjt5c2k/export?format=csv"
-SPREADSHEET_LINK="https://docs.google.com/spreadsheets/d/1YwQjEsfl1H3kDUFMwPjYQLgqeUrrrY0MBp0Q5n4NKhs/export?format=csv"
+##SPREADSHEET_LINK="https://docs.google.com/spreadsheets/d/1RIiyyyTIbz6HRRbVrbnf56rNVvgsLUk3FSJ4xjt5c2k/export?format=csv"
+##SPREADSHEET_LINK="https://docs.google.com/spreadsheets/d/1YwQjEsfl1H3kDUFMwPjYQLgqeUrrrY0MBp0Q5n4NKhs/export?format=csv"
+SPREADSHEET_LINK_NCBI="https://docs.google.com/spreadsheets/d/12LIbdqf7D1Lq7pppJMQw4u2hk9N2CxS5CapoZsJn_us/export?format=csv"
+SPREADSHEET_LINK_INSECT_BASE="https://docs.google.com/spreadsheets/d/12LIbdqf7D1Lq7pppJMQw4u2hk9N2CxS5CapoZsJn_us/export?format=csv&gid=601092575"
 ID_COLUMN="assembly_ID"
+SPECIES_COLUMN="Species name"
 GROUP_COLUMN="group"
 PATH_TO_DATASETS="/homes/pbarth/software/RefSeq_downloader"
+INSECTBASE_LINK_START="http://v2.insect-genome.com/api/Download/..-01_data-01_species-"
+INSECTBASE_LINK_END_GENOME=".genome.fa.tar.bz2"
+INSECTBASE_LINK_END_ANNOTATION=".gff3"
+INSECTBASE_LINK_END_CDS=".cds.fa"
 
 # TODO: Add hash to tmp directory
 # TODO: Add tests if directories already exist
 # TODO: Add argument to potentially not delete the tmp-directory
 # TODO: put unwanted tool outputs to /dev/null
 
+mkdir ${TMP_DIR}
+mkdir ${TMP_DIR_GROUPS}
 
-
-download_data() {
-	mkdir ${TMP_DIR}
+download_data_ncbi() {
+	
+	DOWNLOAD_DIR_TMP="${TMP_DIR}/NCBI"
+	mkdir ${DOWNLOAD_DIR_TMP}
 	# Download spreadsheet as a CSV file
-	curl -L ${SPREADSHEET_LINK} > ${TMP_DIR}/insect_genomes_overview.csv
+	curl -L ${SPREADSHEET_LINK_NCBI} > ${TMP_DIR}/insect_genomes_overview_ncbi.csv
 	# Get all groups from the corresponding column
-	DB_GROUPS=$(awk -v group_col="$GROUP_COLUMN" -F ',' '
+	DB_GROUPS=$(awk -v group_col="${GROUP_COLUMN}" -F ',' '
 		NR>1 {
 			printf $col" "
 		} 
@@ -35,12 +48,12 @@ download_data() {
 				if($i == group_col) col=i
 			}
 		}
-		' "$TMP_DIR"/insect_genomes_overview.csv)
+		' "${TMP_DIR}"/insect_genomes_overview_ncbi.csv)
 	# Make group names unique (only present once in array)
 	declare -a DB_GROUPS_UNIQUE=()
-	for element in ${DB_GROUPS[@]}; do
-		if [[ ! ${DB_GROUPS_UNIQUE[@]} =~ ${element} ]]; then
-			DB_GROUPS_UNIQUE+=($element)
+	for ELEMENT in ${DB_GROUPS[@]}; do
+		if [[ ! ${DB_GROUPS_UNIQUE[@]} =~ ${ELEMENT} ]]; then
+			DB_GROUPS_UNIQUE+=(${ELEMENT})
 		fi
 	done
 	echo "Groups found: ${DB_GROUPS_UNIQUE[@]}"
@@ -57,32 +70,145 @@ download_data() {
 					if($i == id_col) id_index=i
 					if($i == group_col) group_index=i
 				}
-			}' ${TMP_DIR}/insect_genomes_overview.csv)
+			}' ${TMP_DIR}/insect_genomes_overview_ncbi.csv)
 		
 		# Generate directory within tmp to download genomes
-		DIR_GROUP_TMP=${TMP_DIR}/${current_group}
-		mkdir ${DIR_GROUP_TMP}
+		DIR_GROUP_TMP=${TMP_DIR_GROUPS}/${current_group}
+		if [ ! -d "${DIR_GROUP_TMP}" ]; then
+			mkdir ${DIR_GROUP_TMP}
+		fi
+
 		# Download genomes 
-		${PATH_TO_DATASETS}/datasets download genome accession $GENOME_IDS --include gff3,cds,genome,seq-report --dehydrated --filename ${DIR_GROUP_TMP}/insect_genomes.zip
+		${PATH_TO_DATASETS}/datasets download genome accession ${GENOME_IDS} --include gff3,cds,genome,seq-report --dehydrated --filename ${DOWNLOAD_DIR_TMP}/insect_genomes.zip
 		# Unzip downloaded genomes
-		unzip ${DIR_GROUP_TMP}/insect_genomes.zip -d ${DIR_GROUP_TMP}
+		unzip ${DOWNLOAD_DIR_TMP}/insect_genomes.zip -d ${DOWNLOAD_DIR_TMP}
 		# Rehydrate downloaded genomes (necessary for bulk download to restore all files)
-		${PATH_TO_DATASETS}/datasets rehydrate --directory ${DIR_GROUP_TMP}
+		${PATH_TO_DATASETS}/datasets rehydrate --directory ${DOWNLOAD_DIR_TMP}
 		
 		# Generate file to collect all reference genomes
 		PATH_COLLECTED_FASTA="${DIR_GROUP_TMP}/genomes_collected_${current_group}.fna"
-		touch ${PATH_COLLECTED_FASTA}
+		if [ ! -f "${PATH_COLLECTED_FASTA}" ]; then
+			touch ${PATH_COLLECTED_FASTA}
+		fi
 
 		# Go through every genome
 		for GENOME_ID_TMP in ${GENOME_IDS[@]}; do
+			# Generate variable for output dir files with changed names
+			TARGET_DIR="${DIR_GROUP_TMP}/${GENOME_ID_TMP}"
+			TARGET_GENOME="${DIR_GROUP_TMP}/${GENOME_ID_TMP}/${GENOME_ID_TMP}.genome.fa"
+			TARGET_ANNOTATION="${DIR_GROUP_TMP}/${GENOME_ID_TMP}/${GENOME_ID_TMP}.gff3"
+			TARGET_CDS="${DIR_GROUP_TMP}/${GENOME_ID_TMP}/${GENOME_ID_TMP}.cds.fa"
+			mkdir ${TARGET_DIR}
 			# Get path of current accession
-			DIR_CURRENT_ID="${DIR_GROUP_TMP}/ncbi_dataset/data/${GENOME_ID_TMP}"
+			DIR_CURRENT_ID="${DOWNLOAD_DIR_TMP}/ncbi_dataset/data/${GENOME_ID_TMP}"
 			# Get path of genome file
 			GENOME_FILE=$(ls ${DIR_CURRENT_ID}/${GENOME_ID_TMP}*.fna 2>/dev/null )
-			if [ -f "$GENOME_FILE" ]; then
+			
+			if [ -f "${GENOME_FILE}" ]; then
 				echo "Modifying genome ${GENOME_ID_TMP}"
 				# Modify genome file to add the accession to the first string of the header. This way they can be easily assigned in later steps
 				awk -v accession=${GENOME_ID_TMP} '{
+					if (/^>/) {
+						sub(/ /, "_" accession " ", $0)
+					}; print $0
+				}' ${GENOME_FILE} > temp_file.fa && mv temp_file.fa ${TARGET_GENOME}
+				# Copy genome to collect fasta file
+				cat ${TARGET_GENOME} >> ${PATH_COLLECTED_FASTA}
+			else
+				echo "Genome file belonging to accession ${GENOME_ID_TMP} seems to missing"
+			fi
+
+			# Check if an annotation file is present. If yes rename it
+			CURRENT_ANNOTATION_FILE="${DIR_CURRENT_ID}/genomic.gff"
+			if [ -f "${CURRENT_ANNOTATION_FILE}" ]; then
+				echo "Annotation found for ${GENOME_ID_TMP}"
+				cp ${CURRENT_ANNOTATION_FILE} ${TARGET_ANNOTATION}
+			else
+				echo "No annotation found for ${GENOME_ID_TMP}"
+			fi
+
+			# Check if CDS file is present. If yes, rename it
+			CURRENT_CDS_FILE="${DIR_CURRENT_ID}/cds_from_genomic.fna"
+			if [ -f "${CURRENT_CDS_FILE}" ]; then
+				echo "CDS found for ${GENOME_ID_TMP}"
+				cp ${CURRENT_CDS_FILE} ${TARGET_CDS}
+			else
+				echo "No CDS found for ${GENOME_ID_TMP}"
+			fi
+		done
+		echo -e "\n"
+	done
+}
+	
+download_data_insectbase(){
+	##mkdir ${TMP_DIR_INSECTBASE}
+	# Download spreadsheet as a CSV file
+	curl -L ${SPREADSHEET_LINK_INSECT_BASE} > ${TMP_DIR}/insect_genomes_overview_insectbase.csv
+	# Get all groups from the corresponding column
+	DB_GROUPS=$(awk -v group_col="$GROUP_COLUMN" -F ',' '
+		NR>1 {
+			printf $col" "
+		} 
+		NR==1 {
+			for(i=1; i<=NF; i++) {
+				if($i == group_col) col=i
+			}
+		}
+		' "${TMP_DIR}"/insect_genomes_overview_insectbase.csv)
+	# Make group names unique (only present once in array)
+	declare -a DB_GROUPS_UNIQUE=()
+	for element in ${DB_GROUPS[@]}; do
+		if [[ ! ${DB_GROUPS_UNIQUE[@]} =~ ${element} ]]; then
+			DB_GROUPS_UNIQUE+=($element)
+		fi
+	done
+	echo "Groups found: ${DB_GROUPS_UNIQUE[@]}"
+	# Go through every group, download the genomes
+	for current_group in ${DB_GROUPS_UNIQUE[@]}; do
+		echo "Processing insect group ${current_group}"
+		# Extract all species names from the corresponding column which also belong to the group extracted previously and substitute spaces with underscores (_)
+		GENOME_IDS=$(awk -v species_col="${SPECIES_COLUMN}" -v group_col="${GROUP_COLUMN}" -v current_group="${current_group}" -F ',' '
+			NR>1 {
+				if($group_index == current_group) {
+					gsub(/ /, "_", $species_index)
+					printf $species_index" "
+				}
+			} 
+			NR==1 {
+				for(i=1; i<=NF; i++) {
+					if($i == species_col) species_index=i
+					if($i == group_col) group_index=i
+				}
+			}' ${TMP_DIR}/insect_genomes_overview_insectbase.csv)
+
+		# Generate directory within tmp to download genomes
+		DIR_GROUP_TMP="${TMP_DIR_GROUPS}/${current_group}"
+		if [ ! -d "${DIR_GROUP_TMP}" ]; then
+			mkdir ${DIR_GROUP_TMP}
+		fi
+		PATH_COLLECTED_FASTA="${DIR_GROUP_TMP}/genomes_collected_${current_group}.fna"
+
+		for SPECIES in ${GENOME_IDS[@]}; do
+			DIR_CURRENT_SPECIES="${DIR_GROUP_TMP}/${SPECIES}"
+
+			FILE_GENOME_DOWNLOAD="${DIR_CURRENT_SPECIES}/${SPECIES}${INSECTBASE_LINK_END_GENOME}"
+			GENOME_FILE="${DIR_CURRENT_SPECIES}/${SPECIES}.genome.fa"
+			ANNOTATION_FILE="${DIR_CURRENT_SPECIES}/${SPECIES}${INSECTBASE_LINK_END_ANNOTATION}"
+			CDS_FILE="${DIR_CURRENT_SPECIES}/${SPECIES}${INSECTBASE_LINK_END_CDS}"
+			mkdir ${DIR_CURRENT_SPECIES}
+			
+			# Download genome, annotation and CDS files
+			echo "Downloading files for ${SPECIES}"
+			curl -L ${INSECTBASE_LINK_START}${SPECIES}-${SPECIES}${INSECTBASE_LINK_END_GENOME} -o ${FILE_GENOME_DOWNLOAD}
+			curl -L ${INSECTBASE_LINK_START}${SPECIES}-${SPECIES}${INSECTBASE_LINK_END_CDS} -o ${CDS_FILE}
+			curl -L ${INSECTBASE_LINK_START}${SPECIES}-${SPECIES}${INSECTBASE_LINK_END_ANNOTATION} -o ${ANNOTATION_FILE}
+			# unpack genome
+			tar -xjf ${FILE_GENOME_DOWNLOAD} -C ${DIR_CURRENT_SPECIES}
+
+			# Go through every genome and check that the file actually exists
+			if [ -f "${GENOME_FILE}" ]; then
+				echo "Modifying genome ${SPECIES}"
+				awk -v accession=${SPECIES} '{
 					if (/^>/) {
 						sub(/ /, "_" accession " ", $0)
 					}; print $0
@@ -90,23 +216,33 @@ download_data() {
 				# Copy genome to collect fasta file
 				cat ${GENOME_FILE} >> ${PATH_COLLECTED_FASTA}
 			else
-				echo "Genome file belonging to accession ${GENOME_ID_TMP} seems to missing"
+				echo "Genome file belonging to ${SPECIES} seems to missing"
+			fi
+
+			if [ -f "${ANNOTATION_FILE}" ]; then
+				echo "Annotation found for ${SPECIES}"
+			else
+				echo "No annotation found for ${SPECIES}"
+			fi
+
+			if [ -f "${CDS_FILE}" ]; then
+				echo "CDS found for ${SPECIES}"
+			else
+				echo "No CDS found for ${SPECIES}"
 			fi
 		done
 		echo -e "\n"
-	done
-}
-	
-	
-	
+	done	
+}	
 
 
 
 generate_indexes() {
-	mkdir ${OUTPUT_DIR}
 	# Get list of all groups in tmp dir as they were downloaded during download data 
 		# (list entries | filter for dirs | reverse | get first column (dir name) | reverse again | turn newline to space in order to get an array)
-	GROUPS_LIST=$(ls -l ${TMP_DIR} | grep "^d" | rev | cut -d ' ' -f 1 | rev | tr '\n' ' ')
+	GROUPS_LIST=$(ls -l ${TMP_DIR_GROUPS} | grep "^d" | rev | cut -d ' ' -f 1 | rev | tr '\n' ' ')
+
+	mkdir ${OUTPUT_DIR}
 
 	# Generate file that contains paths to DBS
 	PATH_DB_INFO="${OUTPUT_DIR}/info_db_dirs.tsv"
@@ -115,7 +251,7 @@ generate_indexes() {
 
 	# Go through all accessions of the current group
 	for CURRENT_GROUP in ${GROUPS_LIST[@]}; do
-		TMP_DIR_CURRENT_GROUP="${TMP_DIR}/${CURRENT_GROUP}/ncbi_dataset/data"
+		TMP_DIR_CURRENT_GROUP="${TMP_DIR_GROUPS}/${CURRENT_GROUP}"
 		OUTPUT_DIR_CURRENT_GROUP="${OUTPUT_DIR}/${CURRENT_GROUP}"
 		GENOMES_LIST=$(ls -l ${TMP_DIR_CURRENT_GROUP} | grep "^d" | rev | cut -d ' ' -f 1 | rev | tr '\n' ' ')
 		
@@ -141,27 +277,27 @@ generate_indexes() {
 		for CURRENT_ACCESSION in ${GENOMES_LIST[@]}; do
 			TMP_DIR_CURRENT_ACCESSION="${TMP_DIR_CURRENT_GROUP}/${CURRENT_ACCESSION}"
 
-			# Check if annotation is present. If yes it will be renamed into the output dir
-			if [ -f "${TMP_DIR_CURRENT_ACCESSION}/genomic.gff" ]; then
-				echo "Annotation found for ${CURRENT_ACCESSION}"
-				FILE_ANNOTATION="${ANNOTATION_DIR}/${CURRENT_ACCESSION}.gff"
-				cp ${TMP_DIR_CURRENT_ACCESSION}/genomic.gff ${FILE_ANNOTATION}
+			GENOME_FILE="${TMP_DIR_CURRENT_ACCESSION}/${CURRENT_ACCESSION}.genome.fa"
+			ANNOTATION_FILE="${TMP_DIR_CURRENT_ACCESSION}/${CURRENT_ACCESSION}.gff3"
+			CDS_FILE="${TMP_DIR_CURRENT_ACCESSION}/${CURRENT_ACCESSION}.cds.fa"
 
-				echo -e "${CURRENT_ACCESSION}\tTRUE\t$(realpath ${FILE_ANNOTATION})" >> ${PATH_ANNOTATION_INFO}
+			# Check if annotation is present. If yes it will be renamed into the output dir
+			if [ -f "${ANNOTATION_FILE}" ]; then
+				TARGET_FILE="${ANNOTATION_DIR}/${CURRENT_ACCESSION}.gff3"
+				cp ${ANNOTATION_FILE} ${TARGET_FILE}
+
+				echo -e "${CURRENT_ACCESSION}\tTRUE\t$(realpath ${TARGET_FILE})" >> ${PATH_ANNOTATION_INFO}
 			else
-				echo "No annotation found for ${CURRENT_ACCESSION}"
 				echo -e "${CURRENT_ACCESSION}\tFALSE\tNA" >> ${PATH_ANNOTATION_INFO}
 			fi
 
 			# Do the same thing as above for the CDS file
-			if [ -f "${TMP_DIR_CURRENT_ACCESSION}/cds_from_genomic.fna" ]; then
-				echo "CDS found for ${CURRENT_ACCESSION}"
-				FILE_CDS="${CDS_DIR}/CDS_${CURRENT_ACCESSION}.fna"
-				cp ${TMP_DIR_CURRENT_ACCESSION}/cds_from_genomic.fna ${FILE_CDS}
+			if [ -f "${CDS_FILE}" ]; then
+				TARGET_FILE="${CDS_DIR}/${CURRENT_ACCESSION}.cds.fa"
+				cp ${CDS_FILE} ${TARGET_FILE}
 
-				echo -e "${CURRENT_ACCESSION}\tTRUE\t$(realpath ${FILE_CDS})" >> ${PATH_CDS_INFO}
+				echo -e "${CURRENT_ACCESSION}\tTRUE\t$(realpath ${TARGET_FILE})" >> ${PATH_CDS_INFO}
 			else
-				echo "No CDS found for ${CURRENT_ACCESSION}"
 				echo -e "${CURRENT_ACCESSION}\tFALSE\tNA" >> ${PATH_CDS_INFO}
 			fi
 
@@ -170,13 +306,13 @@ generate_indexes() {
 		
 		# Copy fasta file containing all genome chromosomes of the current group
 		FILENAME_COLLECTED_GENOMES="genomes_collected_${CURRENT_GROUP}.fna"
-		FILE_COLLECTED_GENOMES="${TMP_DIR}/${CURRENT_GROUP}/${FILENAME_COLLECTED_GENOMES}"
+		FILE_COLLECTED_GENOMES="${TMP_DIR_GROUPS}/${CURRENT_GROUP}/${FILENAME_COLLECTED_GENOMES}"
 		OUTPUT_FILE_COLLECTED_GENOMES="${OUTPUT_DIR_CURRENT_GROUP}/${FILENAME_COLLECTED_GENOMES}"
 		if [ -f "${FILE_COLLECTED_GENOMES}" ]; then
-			echo "Copying file with collected genomes to output dir: ${FILENAME_COLLECTED_GENOMES})"
+			echo "Copying file with collected genomes to output dir: ${FILENAME_COLLECTED_GENOMES}"
 			cp ${FILE_COLLECTED_GENOMES} ${OUTPUT_FILE_COLLECTED_GENOMES}
 		else
-			echo -e "Something went wrong with the collected genomes file of ${CURRENT_GROUP}.\nThis should be investigated by hand."
+			echo -e "Something went wrong with the collected genomes file of ${CURRENT_GROUP}.\nThis should be investigated manually."
 		fi
 
 		# Build BLASTN DB
@@ -186,8 +322,8 @@ generate_indexes() {
 
 		echo -e "${CURRENT_GROUP}\t${DB_BASENAME}" > ${PATH_DB_INFO}
 		
-
 	done
+}
 
 	#TODO: Get md5 checksums
 	# Get important files
@@ -203,10 +339,8 @@ generate_indexes() {
 	##md5sum "$TMP_DIR"/ncbi_dataset/data/GC{A,F}_*/*.{*_genomic.fna,gff} >> "$OUTPUT_DIR"/metadata/md5sum.tsv
 
 
-}
-
-
 
 # Start functions
-download_data
+download_data_ncbi
+download_data_insectbase
 generate_indexes
